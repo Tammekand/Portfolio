18064e91d332d6bd7d9c51caf7f1ca14f0b1b9c5
Axel-Martin Tammekand <axelmartintammekand@gmail.com>
2024-11-06 17:47:00 +0200

    added vercel SpeedInsights

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
 } from '@remix-run/react';
 import type { LinksFunction } from '@remix-run/node';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/remix';
 
 import './tailwind.css';
 
@@ -39,7 +40,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				{children}
 				<ScrollRestoration />
 				<Scripts />
-				<Analytics />;
+				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
